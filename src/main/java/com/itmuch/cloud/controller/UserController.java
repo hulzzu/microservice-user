f409a5fba3f58f801d060d0630a04cedbf02a355
hqlzzu <1157735700@qq.com>
2018-07-09 11:29:03 +0800

    测试0001

---
+++ src/main/java/com/itmuch/cloud/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.itmuch.cloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping("/hello/{userName}")
+	public String sayHello(@PathVariable String userName) {
+		if(userName==null || "".equals(userName)){
+			return "用户名称怎么能为空呢";
+		}
+		return userName+"，你好！";
+	}
+}
