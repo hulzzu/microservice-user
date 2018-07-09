c8034b721331514ef11ccb2d84c6d7959666a40b
hqlzzu <1157735700@qq.com>
2018-07-09 19:49:52 +0800

    2222

--- src/main/java/com/itmuch/cloud/UserApplication.java
+++ src/main/java/com/itmuch/cloud/UserApplication.java
@@ -11,5 +11,6 @@ public class UserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 		System.out.println("111111111111");
+		System.out.println("12222222222");
 	}
 }
